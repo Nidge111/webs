02fc42b0ebcf620f225a932c0728fbd8f428844e
Nidge111 <133172270+Nidge111@users.noreply.github.com>
2023-06-23 23:29:30 +0100

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const app = express();
 
 app.get('/.well-known/pki-validation/D7475779BF8C3489838FBEADA7719A46.txt', (req, res) => {
-  res.sendFile('./D7475779BF8C3489838FBEADA7719A46.txt', { root: __dirname });
+  res.sendFile('./D7475779BF8C3489838FBEADA7719A46.txt');
 });
 
 const backport = 5000; // Choose a suitable port number
