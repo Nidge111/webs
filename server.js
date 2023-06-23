be4f64e52e36926199c4d5f9de2579976a0804c8
Nidge111 <133172270+Nidge111@users.noreply.github.com>
2023-06-23 23:35:09 +0100

    Update server.js

--- server.js
+++ server.js
@@ -4,11 +4,11 @@ const fs = require('fs');
 
 const app = express();
 
-app.get('/.well-known/pki-validation/D7475779BF8C3489838FBEADA7719A46.txt', (req, res) => {
+app.get('/.well-known/pki-validation/D7475779BF8C3489838FBEADA7719A46.txt', (req, res) -> {
   res.sendFile('/home/ec3-user/webs/D7475779BF8C3489838FBEADA7719A46.txt');
-});
+})
 
-const backport = 5000; // Choose a suitable port number
+const backport = 3000; // Choose a suitable port number
 app.listen(backport, () => {
   console.log(`Server is running on port ${backport}`);
 });
