67114a8a0c184f56bf7af53cc4d9f2a038abd062
Nidge111 <133172270+Nidge111@users.noreply.github.com>
2023-06-27 00:16:30 +0100

    Update server.js

--- server.js
+++ server.js
@@ -11,9 +11,12 @@ app.use(cors());
 
 app.get('/.well-known/pki-validation/11223558199E9123ED7987B4B399C906.txt', (req, res) => {
     console.log ("its working breh");
-    res.sendFile('/home/ec2-user/webs/11223558199E9123ED7987B4B399C906.txt');
+    res.sendFile('ec2-user@ip-172-31-32-186/home/ec2-user/webs/11223558199E9123ED7987B4B399C906.txt');
 });
 
 app.listen(PORT, () => {
   console.log(`Proxy server is running on port ${PORT}`);
 });
+
+
+http://16.16.117.214:3000/home/ec2-user/webs/11223558199E9123ED7987B4B399C906.txt
