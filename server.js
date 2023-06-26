e47fe393f576f2be0bf336bf293ed1684d338950
Nidge111 <133172270+Nidge111@users.noreply.github.com>
2023-06-27 00:10:29 +0100

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ app.use(cors());
 
 
 app.get('/.well-known/pki-validation/11223558199E9123ED7987B4B399C906.txt', (req, res) => {
-    res.sendFile("/home/ec2-user/webs/11223558199E9123ED7987B4B399C906.txt")
+    console.log ("its working breh");
+    res.sendFile('/home/ec2-user/webs/11223558199E9123ED7987B4B399C906.txt');
 });
 
 app.listen(PORT, () => {
