26df31b7908bb2a5cf27b366a370417a1d7391fb
Nidge111 <133172270+Nidge111@users.noreply.github.com>
2023-06-27 21:30:16 +0100

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  dbConfig: {
+    host: 'website.codfvu1pgxsn.eu-north-1.rds.amazonaws.com',
+    port: '3306',
+    user: 'root',
+    password: '9pQ%Y33efD9*3Qfzj$AR*$Jm%XvtWJ3MUx65KMj',
+    database: 'userdata'
+  }
+};
